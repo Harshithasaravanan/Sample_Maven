728c7e72f114b22937bfc660f8f41090a1999d54
Harshitha s <harshithasaravanan25@gmail.com>
2024-11-06 20:36:21 +0530

    Add existing project files to Git

---
+++ src/main/java/Q7/Harshi123/Harshi123Application.java
@@ -0,0 +1,13 @@
+package Q7.Harshi123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Harshi123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Harshi123Application.class, args);
+	}
+
+}
